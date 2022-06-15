0398531b53695cebff29d177ce3ddf21af7575aa
panurjoma <pablohl50@gmail.com>
2022-06-16 00:22:47 +0200

    First commit of FreeRTOS project in the middle of SEGGER integration to debug code

---
+++ 001Tasks/Debug/ThirdParty/FreeRTOS/croutine.d
@@ -0,0 +1,21 @@
+ThirdParty/FreeRTOS/croutine.o: ../ThirdParty/FreeRTOS/croutine.c \
+ D:/RTOS_Workspace/001Tasks/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ D:/RTOS_Workspace/001Tasks/ThirdParty/FreeRTOS/FreeRTOSConfig.h \
+ D:/RTOS_Workspace/001Tasks/ThirdParty/FreeRTOS/include/projdefs.h \
+ D:/RTOS_Workspace/001Tasks/ThirdParty/FreeRTOS/include/portable.h \
+ D:/RTOS_Workspace/001Tasks/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ D:/RTOS_Workspace/001Tasks/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ D:/RTOS_Workspace/001Tasks/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
+ D:/RTOS_Workspace/001Tasks/ThirdParty/FreeRTOS/include/task.h \
+ D:/RTOS_Workspace/001Tasks/ThirdParty/FreeRTOS/include/list.h \
+ D:/RTOS_Workspace/001Tasks/ThirdParty/FreeRTOS/include/croutine.h
+D:/RTOS_Workspace/001Tasks/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+D:/RTOS_Workspace/001Tasks/ThirdParty/FreeRTOS/FreeRTOSConfig.h:
+D:/RTOS_Workspace/001Tasks/ThirdParty/FreeRTOS/include/projdefs.h:
+D:/RTOS_Workspace/001Tasks/ThirdParty/FreeRTOS/include/portable.h:
+D:/RTOS_Workspace/001Tasks/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+D:/RTOS_Workspace/001Tasks/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+D:/RTOS_Workspace/001Tasks/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
+D:/RTOS_Workspace/001Tasks/ThirdParty/FreeRTOS/include/task.h:
+D:/RTOS_Workspace/001Tasks/ThirdParty/FreeRTOS/include/list.h:
+D:/RTOS_Workspace/001Tasks/ThirdParty/FreeRTOS/include/croutine.h:
